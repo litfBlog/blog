d077e69ea36c791e284cc67356da4f47fbc43fb0
Litfa <admin@litf.com.cn>
2022-02-16 17:26:15 +0800

    init

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,17 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'semi': ['error', 'never'],
+    'no-multiple-empty-lines': ['error', { 'max': 1 }],
+    'comma-dangle': ['error', 'never'],
+    'quotes': ['error', 'single'],
+    'spaced-comment': ['error', 'always'],
+    'no-dupe-keys': ['off'],
+
+    camelcase: ['error', { properties: 'never' }],
+    'no-var': 'error',
+    'no-empty': ['error', { allowEmptyCatch: true }],
+
   }
 }
