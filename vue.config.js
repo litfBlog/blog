f3b868520e3189299f9f1471d5326b399b5caf97
Litfa <admin@litf.com.cn>
2022-03-18 18:34:32 +0800

    静态资源代理

--- vue.config.js
+++ vue.config.js
@@ -8,6 +8,11 @@ module.exports = {
     proxy: {
       '/api': {
         target: 'http://127.0.0.1:3000/'
+      },
+      'static': {
+        target: 'http://127.0.0.1:3000/',
+        changeOrigin: true,
+        logLevel: 'debug'
       }
     }
   },
