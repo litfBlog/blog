083b8e22a234e803187767beedb6b3c9a045c207
Litfa <admin@litf.com.cn>
2022-03-21 17:33:33 +0800

    chroe(eslint)

--- .eslintrc.js
+++ .eslintrc.js
@@ -34,7 +34,9 @@ module.exports = {
     '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
 
-    '@typescript-eslint/no-explicit-any': 'off'
+    '@typescript-eslint/no-explicit-any': 'off',
+
+    '@typescript-eslint/no-non-null-assertion': 'off'
 
   }
 }
