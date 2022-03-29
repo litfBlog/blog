ccca7dc83991f11350cb2c40ed53337654efcd36
Litfa <admin@litf.com.cn>
2022-03-29 14:31:31 +0800

    fix(html2text): 优化形参类型

--- src/utils/html2text.ts
+++ src/utils/html2text.ts
@@ -1,35 +1,31 @@
 /*
  * @Author: litfa
  * @Date: 2022-03-28 12:07:42
- * @LastEditTime: 2022-03-28 14:20:45
+ * @LastEditTime: 2022-03-29 14:30:14
  * @LastEditors: litfa
  * @Description: html字符换提取内容文字
  * @FilePath: /blog/src/utils/html2text.ts
  * 
  */
-interface options {
-  warp?: boolean
-  length?: number
-}
 
 /**
- * @description: 
+ * @description: html字符换提取内容文字
  * @param {string} htmlString
  * @param {options} options
  * @return {string}
  */
-export default (htmlString: string, options?: options): string => {
+export default (htmlString: string, { warp = true, length }: { warp: boolean, length?: number }): string => {
   // 创建 dom
   const div = document.createElement('div')
   // 将html字符串 放到dom里
   div.innerHTML = htmlString
   // 去除换行
-  if (options?.warp == false) {
+  if (warp == false) {
     div.innerText = div.innerText.replaceAll('\r', ' ')
   }
   // 截取长度
-  if (options?.length) {
-    div.innerText = div.innerText.substring(0, options.length)
+  if (length) {
+    div.innerText = div.innerText.substring(0, length)
   }
 
   return div.innerText
