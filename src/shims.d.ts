c5a4b7dbe4772b2bd9de8a5ffc7d31064361eecf
Litfa <admin@litf.com.cn>
2022-04-06 18:49:52 +0800

    feat(package): eventbus

--- src/shims.d.ts
+++ src/shims.d.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: litfa
  * @Date: 2022-03-13 17:37:28
- * @LastEditTime: 2022-03-13 17:42:49
+ * @LastEditTime: 2022-04-06 18:44:51
  * @LastEditors: litfa
  * @Description: 
  * @FilePath: /blog/src/shims.d.ts
@@ -11,3 +11,4 @@
 declare module '@kangc/v-md-editor'
 declare module '@kangc/v-md-editor/lib/theme/vuepress.js'
 declare module 'prismjs'
+declare module 'vue3-eventbus'
