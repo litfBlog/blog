453924f704f98b375337b64aed8dd6b98c0c746b
Litfa <admin@litf.com.cn>
2022-04-09 17:36:52 +0800

    feat(HomeComponents/Footer): fooder

---
+++ src/components/HomeComponents/Footer.vue
@@ -0,0 +1,123 @@
+<!--
+ * @Author: litfa
+ * @Date: 2022-04-08 18:56:38
+ * @LastEditTime: 2022-04-09 17:35:54
+ * @LastEditors: litfa
+ * @Description: 底部信息
+ * @FilePath: /blog/src/components/HomeComponents/Footer.vue
+ * 
+-->
+<script lang="ts" setup>
+import dayjs from 'dayjs'
+const icons = ['qq', '微信', 'github', 'email']
+</script>
+
+<template>
+  <div class="Footer">
+    <div class="copyright">©2021-{{ dayjs().format('YYYY') }} litfa all rights reserved.</div>
+    <div class="icp">
+      <a href="https://beian.miit.gov.cn/">豫ICP备2021024510号-2</a>
+      <a href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=4101820200807">
+        <img src="./../../assets/icp.png" />
+        豫公网安备 4101820200807号
+      </a>
+    </div>
+    <div class="contact">
+      <el-popover placement="top-start" :width="200" trigger="hover">
+        <template #reference>
+          <div class="icon" style="background-color: #12b7f5"></div>
+        </template>
+        <div class="popover-box">
+          <a
+            href="https://qm.qq.com/cgi-bin/qm/qr?k=KhU1Ifs9GAuPaHfsGMHM4nUst9pR58Uj&noverify=0"
+          >1585380249</a>
+          <img src="./../../assets/qq.png" alt="qq" />
+        </div>
+      </el-popover>
+      <el-popover placement="top-start" :width="200" trigger="hover">
+        <template #reference>
+          <div class="icon" style="background-color: #2aae67;"></div>
+        </template>
+        <div class="popover-box">
+          Litfa_
+          <img src="./../../assets/wx.png" alt="wx" />
+        </div>
+      </el-popover>
+      <el-popover placement="top-start" :width="200" trigger="hover">
+        <template #reference>
+          <div class="icon" style="background-color: #000;"></div>
+        </template>
+        <a href="https://github.com/litfa" target="_black">https://github.com/litfa</a>
+      </el-popover>
+      <el-popover placement="top-start" :width="200" trigger="hover">
+        <template #reference>
+          <div class="icon" style="background-color: #ecb10c;"></div>
+        </template>
+        <a href="mailto:admin@litf.com.cn">admin@litf.com.cn</a>
+      </el-popover>
+    </div>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.Footer {
+  font-size: 14px;
+  word-break: break-all;
+  flex-wrap: wrap;
+  color: #000a;
+  .icp {
+    display: flex;
+    flex-direction: column;
+    // align-items: center;
+    a {
+      display: flex;
+      align-items: center;
+    }
+  }
+  .contact {
+    display: flex;
+    justify-content: space-between;
+    padding-right: 10px;
+    box-sizing: border-box;
+    .icon {
+      width: 35px;
+      height: 35px;
+      background-color: blue;
+      border-radius: 10px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      &::after {
+        content: "";
+        width: 25px;
+        height: 25px;
+        display: block;
+        background-image: url(./../../assets/icon.png);
+        background-size: 25px;
+      }
+      &:nth-child(1)::after {
+        background-position-y: 0;
+      }
+      &:nth-child(2)::after {
+        background-position-y: -25px;
+      }
+      &:nth-child(3)::after {
+        background-position-y: -50px;
+      }
+      &:nth-child(4)::after {
+        background-position-y: -75px;
+      }
+    }
+  }
+}
+.popover-box {
+  display: flex;
+  justify-content: space-around;
+  flex-direction: column;
+  align-items: center;
+  font-size: 20px;
+  img {
+    width: 80%;
+  }
+}
+</style>
--- src/components/HomeComponents/HomeComponents.vue
+++ src/components/HomeComponents/HomeComponents.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: litfa
  * @Date: 2022-03-30 19:30:56
- * @LastEditTime: 2022-03-30 19:46:35
+ * @LastEditTime: 2022-04-08 18:59:53
  * @LastEditors: litfa
  * @Description: 首页侧边组件
  * @FilePath: /blog/src/components/HomeComponents/HomeComponents.vue
@@ -9,14 +9,21 @@
 -->
 <script lang="ts" setup>
 import Miniprogram from './Miniprogram.vue'
-const components = [Miniprogram]
+import Footer from './Footer.vue'
+const components = [Miniprogram, Footer]
 </script>
 
 <template>
-  <component v-for="(i, index) in components" :key="index" :is="i" class="component"></component>
+  <div class="HomeComponents">
+    <component v-for="(i, index) in components" :key="index" :is="i" class="component"></component>
+  </div>
 </template>
 
 <style lang="less" scoped>
+.HomeComponents {
+  display: flex;
+  flex-direction: column;
+}
 .component {
   margin-top: 10px;
 }
