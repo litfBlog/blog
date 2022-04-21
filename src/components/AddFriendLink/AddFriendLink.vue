0f3eb7673db69aeeb2bc5e5dea419f790ec23902
Litfa <admin@litf.com.cn>
2022-04-21 15:09:40 +0800

    feat(AddFriendLink): 表单验证

--- src/components/AddFriendLink/AddFriendLink.vue
+++ src/components/AddFriendLink/AddFriendLink.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: litfa
  * @Date: 2022-04-20 20:33:03
- * @LastEditTime: 2022-04-21 14:15:24
+ * @LastEditTime: 2022-04-21 15:08:02
  * @LastEditors: litfa
  * @Description: 申请友链
  * @FilePath: /blog/src/components/AddFriendLink/AddFriendLink.vue
@@ -18,28 +18,50 @@ const form = reactive({
   desc: '',
   avatar: ''
 })
+const rules = reactive({
+  name: [
+    { required: true, message: '请填写网站名', trigger: 'blur' },
+    { min: 2, max: 10, message: '长度为3~5个字符', trigger: 'blur' }
+  ],
+  url: [
+    { required: true, message: '请填写首页地址', trigger: 'blur' },
+    { min: 4, max: 50, message: '长度为4~50个字符', trigger: 'blur' }
+  ]
+})
 
-const onSubmit = () => {
+const ruleFormRef = ref<any>()
+
+const onSubmit = async (formEl: any) => {
   console.log('submit!')
+  if (!formEl) return
+  await formEl.validate((valid: any, fields: any) => {
+    if (valid) {
+      console.log('submit!')
+    } else {
+      console.log('error submit!', fields)
+    }
+  })
 }
 </script>
 
 <template>
   <div>
-    <el-button type="success" @click="open = !open">申请友链</el-button>
+    <el-button type="success" @click="open = !open">添加友链</el-button>
     <div style="margin-top: 20px; height: 200px">
       <el-collapse-transition>
         <div class="form" v-show="open">
           <el-form
+            ref="ruleFormRef"
             :model="form"
+            :rules="rules"
             label-width="100px"
             label-position="right"
             class="demo-form-inline"
           >
-            <el-form-item label="网站名">
+            <el-form-item label="网站名" prop="name">
               <el-input v-model="form.name" />
             </el-form-item>
-            <el-form-item label="主页地址">
+            <el-form-item label="主页地址" prop="url">
               <el-input v-model="form.url" />
             </el-form-item>
             <el-form-item label="介绍">
@@ -50,7 +72,7 @@ const onSubmit = () => {
             </el-form-item>
 
             <el-form-item>
-              <el-button type="primary" @click="onSubmit">Query</el-button>
+              <el-button type="primary" @click="onSubmit(ruleFormRef)">提交</el-button>
             </el-form-item>
           </el-form>
           <div class="preview">
