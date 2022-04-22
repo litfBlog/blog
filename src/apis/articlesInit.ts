9f67f3669c483a72df7ab217845ac98126c16f9c
Litfa <admin@litf.com.cn>
2022-04-22 18:20:14 +0800

    refactor(Edit/init): 同步init api修改

--- src/apis/articlesInit.ts
+++ src/apis/articlesInit.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: litfa
  * @Date: 2022-03-11 16:34:52
- * @LastEditTime: 2022-03-11 18:56:52
+ * @LastEditTime: 2022-04-22 18:20:09
  * @LastEditors: litfa
  * @Description: 初始化文章
  * @FilePath: /blog/src/apis/articlesInit.ts
@@ -11,6 +11,6 @@ import request from '@/utils/request'
 export default (): any => {
   return request({
     method: 'POST',
-    url: '/articles/init/add'
+    url: '/v2/articles/init/add'
   })
 }
